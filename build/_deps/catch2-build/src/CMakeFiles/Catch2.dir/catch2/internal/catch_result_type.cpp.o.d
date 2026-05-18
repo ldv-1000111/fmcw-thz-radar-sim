b559cfcb9ee13d0455dc8ded231583dbc87d5905
lvs <ldviveros@gmail.com>
2026-05-18 06:55:22 -0400

    chore: add .gitignore, remove build artifacts from tracking

--- build/_deps/catch2-build/src/CMakeFiles/Catch2.dir/catch2/internal/catch_result_type.cpp.o.d
+++
@@ -1,4 +0,0 @@
-_deps/catch2-build/src/CMakeFiles/Catch2.dir/catch2/internal/catch_result_type.cpp.o: \
- /home/lvs/data/1_devel/10_claudeCode/05_fmcw/fmcw-thz-radar-sim/build/_deps/catch2-src/src/catch2/internal/catch_result_type.cpp \
- /usr/include/stdc-predef.h \
- /home/lvs/data/1_devel/10_claudeCode/05_fmcw/fmcw-thz-radar-sim/build/_deps/catch2-src/src/catch2/../catch2/internal/catch_result_type.hpp
